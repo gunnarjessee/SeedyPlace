76a650c99df94c103f2094715285a6f01fee004a
Gunnar Jessee <gunnarjessee@gmail.com>
2023-07-02 19:52:25 -0400

    updated PlantDropEventListener
    change how itemEntity tracked time and refactored the entire class; oak sapling turns into diamond

--- src/main/java/lostboy/seedyplace/events/PlantDropEventListener.java
+++ src/main/java/lostboy/seedyplace/events/PlantDropEventListener.java
@@ -1,14 +1,17 @@
 package lostboy.seedyplace.events;
 
 import lostboy.seedyplace.SeedyPlaceMod;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
@@ -29,38 +32,39 @@ import java.util.concurrent.TimeUnit;
 public class PlantDropEventListener {
     // ScheduledExecutorService to be remove and replaced.
     // TODO: Add a time checked when a entity loads into the world and see if its plantable. 
-    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
     public void initialize() {
         ServerEntityEvents.ENTITY_LOAD.register(((entity, world) -> {
+
+
             if (entity instanceof ItemEntity) {
-                ItemEntity item = (ItemEntity) entity;
-                ItemStack itemStack = item.getStack();
-                ServerScheduler serverScheduler = ServerScheduler.INSTANCE;
-                if (itemStack.getItem() == Items.OAK_SAPLING) {
-                    executorService.schedule(() -> {
+                ItemEntity itemEntity = (ItemEntity) entity;
 
-                        double x = item.getX();
-                        double y = item.getY();
-                        double z = item.getZ();
-                        int blockX = (int) Math.floor(x);
-                        int blockY = (int) Math.floor(y) - 1; // Check the block below the item
-                        int blockZ = (int) Math.floor(z);
+                if (itemEntity.getStack().getItem() == Items.OAK_SAPLING) {
 
-                        Block block = world.getBlockState(new BlockPos(blockX, blockY + 1, blockZ)).getBlock();
-                        SeedyPlaceMod.LOGGER.debug("Attempting to Place Sampling");
-                        if (block == Blocks.DIRT || block == Blocks.GRASS_BLOCK) {
-                            // Place the sapling on the ground
-                            world.setBlockState(new BlockPos(blockX, blockY, blockZ), Blocks.OAK_SAPLING.getDefaultState());
+                    ServerTickEvents.START_SERVER_TICK.register(server -> {
 
-                            // Remove the item entity
-                            item.remove(Entity.RemovalReason.DISCARDED);
+                        if (entity.age >= 20 * 4 && entity.isAlive()) {
+                            entity.remove(Entity.RemovalReason.DISCARDED);
+                            sTestSpawn(world, new Vec3d(entity.getX(), entity.getY(), entity.getZ()));
                         }
+                    });
 
-                    }, 1, TimeUnit.SECONDS);
+
 
                 }
+
 
             }
         }));
     }
+
+    public void sTestSpawn(World world, Vec3d position) {
+
+        ItemStack itemStack = new ItemStack(Items.DIAMOND, 1);
+        ItemEntity itemEntity = new ItemEntity(world, position.x, position.y, position.z, itemStack);
+
+        world.spawnEntity(itemEntity);
+
+    }
+
 }
